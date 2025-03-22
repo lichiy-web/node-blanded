5429cf6d051ae4f313a80ca7a84915feed57f7c1
Stepannovv <stepannovv@gmail.com>
2025-03-22 14:19:15 +0100

    add getProductsByMinPrice

--- src/scripts/getProductsByMinPrice.js
+++ src/scripts/getProductsByMinPrice.js
@@ -1,4 +1,16 @@
+import { PATH_DB } from "../constants/pathDb.js";
+import fs from "node:fs/promises";
 
 async function getProductsByMinPrice(price) {
-    const productList = 
+  try {
+    const productsData = await fs.readFile(PATH_DB, "utf-8");
+    const productList = JSON.parse(productsData);
+    const filtredProducts = productList.filter(
+      (product) => product.price >= price
+    );
+    console.table(filtredProducts);
+  } catch (error) {
+    console.error(error);
+  }
 }
+getProductsByMinPrice(300);
