d6a28b0b0790a43f79139eb0c78f51ee05cdef98
Stepannovv <stepannovv@gmail.com>
2025-03-22 15:52:38 +0100

    getCategories

--- src/scripts/generateProducts.js
+++ src/scripts/generateProducts.js
@@ -3,15 +3,15 @@ import fs from "node:fs/promises";
 import { createFakeProduct } from "../utils/createFakeProduct.js";
 
 async function generateProducts(number) {
-    try {
-        const productListStr = await fs.readFile(PATH_DB, 'utf-8');
-        const productList = JSON.parse(productListStr);
-        const newProductList = Array(number).fill(0).map(createFakeProduct);
-        const mergedProductList = [...productList, ...newProductList];
-        await fs.writeFile(PATH_DB, JSON.stringify(mergedProductList, null, 2));
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const productListStr = await fs.readFile(PATH_DB, "utf-8");
+    const productList = JSON.parse(productListStr);
+    const newProductList = Array(number).fill(0).map(createFakeProduct);
+    const mergedProductList = [...productList, ...newProductList];
+    await fs.writeFile(PATH_DB, JSON.stringify(mergedProductList, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-generateProducts(5);
+generateProducts(20);
