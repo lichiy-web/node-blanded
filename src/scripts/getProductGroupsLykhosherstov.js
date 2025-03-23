e7317cadf33ac0252b3e62e653d4d64480f40274
Eugene Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-03-23 13:54:20 +0200

    add productGroups3

--- src/scripts/getProductGroupsLykhosherstov.js
+++ src/scripts/getProductGroupsLykhosherstov.js
@@ -28,10 +28,30 @@ const getProductGroups = async () => {
     }, {});
     const productGroupsDuration2 = new Date() - dateMark2;
 
+    const dateMark3 = new Date();
+    const productGroups3 = products.reduce(
+      (groups, { category, name }, i, { length: last }) => {
+        const group = groups[category];
+        !group ? (groups[category] = { [name]: null }) : (group[name] = null);
+
+        if (i === last - 1) {
+          for (let cat in groups) {
+            groups[cat] = Object.keys(groups[cat]);
+          }
+        }
+
+        return groups;
+      },
+      {}
+    );
+    const productGroupsDuration3 = new Date() - dateMark3;
+
     // console.log('productGroups1 = ', productGroups1);
-    console.log('productGroups2 = ', productGroups2);
+    // console.log('productGroups2 = ', productGroups2);
+    console.log('productGroups3 = ', productGroups3);
     console.log('productGroupsDuration1 = ', productGroupsDuration1);
     console.log('productGroupsDuration2 = ', productGroupsDuration2);
+    console.log('productGroupsDuration3 = ', productGroupsDuration3);
   } catch (error) {
     console.error(error);
   }
