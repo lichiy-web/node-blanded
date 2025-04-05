6f5793629ecc1b2d0844b0fcba259e0a20da574d
Stepannovv <stepannovv@gmail.com>
2025-04-05 15:45:21 +0200

    all tasks

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,9 +1,22 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getAllProductsController } from '../controllers/products.js';
+import {
+  getAllProductsController,
+  addNewProductController,
+  getProductByIdController,
+  deleteProductByIdController,
+  updateProductController,
+} from '../controllers/products.js';
 
 const router = new Router();
 
 router.get('/products', ctrlWrapper(getAllProductsController));
 
+router.get('/products/:productId', ctrlWrapper(getProductByIdController));
+
+router.post('/products', ctrlWrapper(addNewProductController));
+
+router.delete('/products/:productId', ctrlWrapper(deleteProductByIdController));
+
+router.patch('/products/:productId', ctrlWrapper(updateProductController));
 export default router;
--- src/services/products.js
+++ src/services/products.js
@@ -1,3 +1,14 @@
 import { ProductCollection } from '../db/models/Product.js';
 
 export const getAllProducts = () => ProductCollection.find();
+
+export const getProductById = (id) => ProductCollection.findById(id);
+
+export const addNewProduct = (productData) =>
+  ProductCollection.create(productData);
+
+export const deleteProductById = (id) =>
+  ProductCollection.findByIdAndDelete(id);
+
+export const updateProduct = (id, productData) =>
+  ProductCollection.findByIdAndUpdate(id, productData, { new: true });
